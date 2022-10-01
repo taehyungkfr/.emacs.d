c0428b42ed4dbdc22f2a4dd2f0b148f0d7a4c9f5
taehyung <taehyungkfr@gmail.com>
2022-10-01 20:02:26 +0900

    Init

---
+++ early-init.el
@@ -0,0 +1,10 @@
+(when (and (fboundp 'native-comp-available-p)
+           (native-comp-available-p))
+  (progn
+    (message "Native comp is available")
+    (setq native-comp-async-report-warnings-errors nil
+          native-comp-deferred-compilation-deny-list '("my.el" "pers.el")
+          comp-deferred-compilation t
+          package-native-compile t)
+    (add-to-list 'native-comp-eln-load-path
+                 (expand-file-name "eln-cache" user-emacs-directory))))
---
+++ init.el
@@ -0,0 +1,30 @@
+;;; init.el --- My Emacs configuration -*- lexical-binding: t; -*-
+(set-language-environment "UTF-8")
+(setq gc-cons-percentage 0.6
+      gc-cons-threshold most-positive-fixnum
+      read-process-output-max (* 1024 1024))
+
+(let ((default-directory user-emacs-directory)
+      (file-name-handler-alist nil))
+
+  ;; 기본 설정파일 로딩
+  (let* ((.org "my.org")
+         (.el "my.el")
+         (mtime (file-attribute-modification-time (file-attributes .org))))
+    (require 'org-compat)
+    (require 'org-macs)
+    (unless (org-file-newer-than-p .el mtime)
+      (require 'ob-tangle)
+      (message "Rewrite my config")
+      (org-babel-tangle-file .org .el "emacs-lisp"))
+    (load-file .el))
+
+  ;; 개인 설정
+  (when (file-exists-p "pers.el")
+    (load-file "pers.el"))
+
+  ;; 기본 디렉토리 변경
+  (cd "~")
+
+  ;; 로딩 이후 가비지컬렉션
+  (garbage-collect))
